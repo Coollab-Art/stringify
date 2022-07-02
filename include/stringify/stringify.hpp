83bf6fc97a93d54eb16857aeb04970e6615f95f5
Jules Fouchy <jules.fouchy@ntymail.com>
2022-07-02 10:59:27 +0200

    🐛 Fix stringification for booleans

--- include/stringify/stringify.hpp
+++ include/stringify/stringify.hpp
@@ -12,13 +12,6 @@ auto stringify(const T&) -> std::string
     return std::string{"[Cool::stringify] ERROR: Couldn't find a to_string() function for this type: "} + typeid(T).name();
 }
 
-// Proper to_string for booleans
-template<>
-inline auto stringify(const bool& value) -> std::string
-{
-    return value ? "true" : "false";
-}
-
 // Ranges implementation
 template<std::ranges::range T>
 auto stringify(const T& value) -> std::string
@@ -39,5 +32,14 @@ auto stringify(const T& value) -> std::string
 }
 
 #include "../generated/all_ways_of_finding_to_string.inl" // Must be after the declaration of `stringify()`, otherwise some concepts fail because they don't know about `stringify()`.
+
+// Proper to_string for booleans
+template<>
+inline auto stringify(const bool& value) -> std::string
+{
+    return value
+               ? "true"
+               : "false";
+}
 
 } // namespace Cool
